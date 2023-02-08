d4023bdc6b332372f7e7ad8d5743c9fc2a17e31e
Cai <s7608130@bns.bns>
2023-02-08 15:04:08 -0500

    peter Added releasenotes

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,20 @@
+import java.io.*;
+import groovy.io.*;
+
+def call(Map config=[:]) {
+    // def dir = new File(/C:\Users\s7608130\Downloads\test\jenkins\JenkinsGroovy\ConsoleApp1/)
+    def dir = new File(pwd());
+
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
+    {
+        writer ->
+            dir.eachFileRecurse(FileType.ANY) { file ->
+                if (file.isDirectory()) {
+                    writer.writeLine(file.name + '/');
+                }
+                else {
+                    writer.writeLine(file.name + '\t' + file.length());
+                }
+            }
+    }
+}
