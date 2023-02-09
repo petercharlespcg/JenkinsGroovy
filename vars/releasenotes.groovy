37f46a2a5e69906583bd8f8e588f3e355cdc47be
Cai <s7608130@bns.bns>
2023-02-09 15:57:55 -0500

    peter Added date

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,5 +1,7 @@
 import java.io.*;
 import groovy.io.*;
+import java.util.Calendar.*;
+import java.text.SimpleDateFormat
 
 @NonCPS
 def call(Map config=[:]) {
@@ -18,6 +20,11 @@ def call(Map config=[:]) {
                 }
             }
     }
+
+    def date = new Date()
+    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+    echo "Date and Time IS: " sdf.format(date)
+    
 
     if (config.changes != "false") {
         echo "changes";
